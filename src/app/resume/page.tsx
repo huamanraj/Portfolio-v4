67ba199b8cedb84efcaa22e559e9ba3f2e584774
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-03-03 02:31:09 +0530

    resume route

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function ResumePage() {
+  redirect(
+    "https://drive.google.com/file/d/1Wl0YWrXSGfQsXE9SxhHtI_PHfAK3ucN9/view?usp=sharing"
+  );
+}
