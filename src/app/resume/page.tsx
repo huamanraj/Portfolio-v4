09c6884763127e182a78b8a8fba848da3ac78ddf
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-03-27 20:31:30 +0530

    resume update

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation";
 
 export default function ResumePage() {
   redirect(
-    "https://drive.google.com/file/d/1Wl0YWrXSGfQsXE9SxhHtI_PHfAK3ucN9/view?usp=sharing"
+    "https://drive.google.com/file/d/1Nn8j84eSs0r-7tPPImX6FWKQNxvfuqrF/view?usp=sharing"
   );
 }
