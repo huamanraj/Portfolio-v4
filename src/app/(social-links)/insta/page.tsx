3d22d00ecb544c0e6b602c4493723050af030020
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-05-03 17:06:15 +0530

    social links and resume update

---
+++ src/app/(social-links)/insta/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function InstaPage() {
+  redirect("https://www.instagram.com/amanraj.me/");
+}
--- src/app/(social-links)/resume/page.tsx
+++ src/app/(social-links)/resume/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation";
 
 export default function ResumePage() {
   redirect(
-    "https://drive.google.com/file/d/1Nn8j84eSs0r-7tPPImX6FWKQNxvfuqrF/view?usp=sharing"
+    "https://drive.google.com/file/d/1iWexr94-nCZrBKImq3k9p610dhZp-nis/view?usp=sharing"
   );
 }
